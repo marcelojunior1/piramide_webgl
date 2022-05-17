fdbb2b7f532008e4c5b7f8cee584f5e9f219abe5
marcelojunior1 <marcelojunior@usp.br>
2022-05-17 16:51:27 -0300

    Piramide aparece no canvas.

--- piramide.js
+++ piramide.js
@@ -1,13 +1,38 @@
 window.onload = main;
 
 // VARIAVEIS GLOBAIS
+const gFov = 30
+
 var gl;
 var gCanvas;
 var gShader = {};
 var gCores = [];
 var gBufferCor;
+var gBufferVertices
+var gIndiceBuffer
+var programInfo;
 
-var gaPositions = [
+var cubeRotation = 0.0;
+
+var indices = [
+    0,1,2,
+    3,4,5,
+    6,7,8,
+    9,10,11,
+    12,13,14,
+    15,16,17
+];
+
+var cores_faces = [
+    [1.0, 0.0, 0.0, 1.0],
+    [0.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [0.0, 0.0, 0.0, 1.0],
+    [0.0, 0.0, 0.0, 1.0],
+];
+
+var gPositions = [
     //Faces
     0,0,1,
     1,0,0,
@@ -26,39 +51,118 @@ var gaPositions = [
     1,0,0,
 
     //Base
-    0,-1,0,
     -1,0,0,
     1,0,0,
+    0,-1,0,
 
-    0,1,0,
+    -1,0,0,
     1,0,0,
-    -1,0,0
-];
-
-var cores_faces = [
-    [1.0, 1.0, 1.0, 1.0],
-    [1.0, 1.0, 0.0, 1.0],
-    [0.0, 1.0, 0.0, 1.0],
-    [0.0, 0.0, 1.0, 1.0],
-    [1.0, 1.0, 0.0, 1.0],
-    [1.0, 1.0, 0.0, 1.0]
+    0,1,0
 ];
 
 function main()
 {
     // Inicia o canvas
-
     gCanvas = document.getElementById("glcanvas");
     gl = gCanvas.getContext('webgl2');
     if (!gl) alert("WebGL 2.0 não está disponível! ");
 
+
     inicia_shaders();
-    desenhe();
+
+    var then = 0;
+
+    function render(now)
+    {
+        now *= 0.0001;
+        const deltaTime = now - then;
+        then = now;
+
+        desenhe(deltaTime);
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
 }
 
 
-function desenhe()
+function desenhe(delta)
 {
+    console.log("OK")
+    gl.clearColor(0.1, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Matriz de pespectiva
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const near= 0.1;
+    const far = 100.0;
+    const projectionMatrix = perspective(gFov,aspect,near,far)
+
+    // Matriz Model View
+    const eye = vec3(3,3,3)
+    const at =  vec3(2,2,2)
+    const up =  vec3(1,1,4)
+    const modelViewMatrix = lookAt( eye, at, up );
+
+    // Leitura dos vertices
+    {
+        const numComponents = 3;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, gBufferVertices);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+    }
+
+    // Leitura das cores
+    {
+        const numComponents = 4;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, gBufferCor);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexColor,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
+    }
+
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gIndiceBuffer);
+
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        flatten(projectionMatrix));
+
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.modelViewMatrix,
+        false,
+        flatten(modelViewMatrix));
+
+    {
+        const vertexCount = indices.length;
+        const type = gl.UNSIGNED_SHORT;
+        const offset = 0;
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+    }
 
 }
 
@@ -69,19 +173,47 @@ function inicia_shaders()
     gl.useProgram(gShader.program);
 
     // Inicia o buffer de vertices na GPU
-    var bufVertices = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufVertices);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(gaPositions), gl.STATIC_DRAW);
+    gBufferVertices = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, gBufferVertices);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(gPositions), gl.STATIC_DRAW);
 
     // Inicia as cores
 
+    var colors = [];
+    for (var j = 0; j < cores_faces.length; ++j)
+    {
+        const c = cores_faces[j];
+        colors = colors.concat(c, c, c, c);
+    }
+
     gBufferCor = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, gBufferCor);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(gCores), gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW)
+
     var aColorLoc = gl.getAttribLocation(gShader.program, "aColor");
     gl.vertexAttribPointer(aColorLoc, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aColorLoc);
     gShader.uResolution = gl.getUniformLocation(gShader.program, "uResolution");
+
+    // Indexando as faces
+    gIndiceBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gIndiceBuffer);
+
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+     programInfo = {
+        program: gShader.program,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(gShader.program, 'aPosition'),
+            vertexColor: gl.getAttribLocation(gShader.program, 'aColor'),
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(gShader.program, 'uProjectionMatrix'),
+            modelViewMatrix: gl.getUniformLocation(gShader.program, 'uModelViewMatrix'),
+        }
+    };
+
+
 }
 
 // --------------------------------------------------------------------------
@@ -103,6 +235,7 @@ gsVertexShaderSrc = `
   `;
 
 gsFragmentShaderSrc = `
+    precision highp float;
     varying lowp vec4 vColor;
 
     void main(void) {
